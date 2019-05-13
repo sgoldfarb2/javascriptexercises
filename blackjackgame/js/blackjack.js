1060e51ff0b1758ad77db3ae13ede9b499811e4e
Sabrina Goldfarb <s.goldfarb2@gmail.com>
2019-05-13 18:55:08 -0500

    continuing the blackjack game

--- blackjackgame/js/blackjack.js
+++ blackjackgame/js/blackjack.js
@@ -3,6 +3,9 @@ let suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
 let dealerHand = []
 var playerHand = []
 var playerPoints = document.getElementById('player-points')
+var total = 0;
+var dealerTotal = 0;
+var playerTotal = 0;
 
 suits.forEach(function (element) {
   for (let i = 2; i < 15; i++) {
@@ -37,6 +40,11 @@ document.getElementById("deal-button").addEventListener("click", function () {
   var dealerPoints = document.getElementById('dealer-points')
 
   dealerPoints.textContent = calculatePoints(dealerHand)
+  dealerTotal = total
+  total = 0;
+  console.log(dealerTotal)
+  console.log(total)
+
 
 
   let card3 = Math.floor(Math.random() * 50)
@@ -52,6 +60,8 @@ document.getElementById("deal-button").addEventListener("click", function () {
 
 
   playerPoints.textContent = calculatePoints(playerHand)
+  playerTotal = calculatePoints(playerHand)
+  total = 0;
 
 
 
@@ -70,31 +80,50 @@ document.getElementById("hit-button").addEventListener("click", function () {
 
   newCard.style = "height: 150px"
   var ourPlayerHand = document.getElementById('player-hand')
+
   ourPlayerHand.appendChild(newCard)
   playerPoints.textContent = calculatePoints(playerHand)
+  total = 0;
 });
 
 
 
-// document.getElementById("btnStand").addEventListener("click", function () {
-//   //write "Stand" logic here
-// });
+document.getElementById('stand-button').addEventListener("click", function () {
+  //write "Stand" logic here
+  console.log(dealerTotal)
+  if (dealerTotal < 17) {
+    var dealerNextCard = document.createElement('img')
+    let nextCard = Math.floor(Math.random() * 48)
+    dealerNextCard.setAttribute('src', deckOfCards[nextCard].imageURL)
+    dealerHand.push(deckOfCards[nextCard])
+    deckOfCards.splice(nextCard, 1)
+    dealerNextCard.style = "height: 150px;"
+    var ourDealerHand = document.getElementById('dealer-hand')
+    ourDealerHand.appendChild(dealerNextCard)
+    var dealerPoints = document.getElementById('dealer-points')
+    dealerPoints.textContent = calculatePoints(dealerHand)
+  } else {
+    return dealerTotal
+  }
+});
 
 
 function calculatePoints(arr) {
-  let total = 0
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].points > 11) {
       total += 10
+
     } else {
       total += arr[i].points
+
     }
 
   }
   if (total > 21) {
-    return "Bust!"
+    return "Bust! You lose!"
   } else {
     return total
 
   }
+
 }
